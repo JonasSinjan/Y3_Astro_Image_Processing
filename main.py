c2d520c4eb9e3de369d51044135e8684c855d664
Robert King <robertkng104@gmail.com>
2019-03-07 14:56:35 +0000

    added histogram

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from astropy.io import fits
+import numpy as np
+import matplotlib.pyplot as plt
+data_points = np.array([])
 
 with fits.open("A1_mosaic.fits") as hdulist:
-    for key,val in hdulist[0].header.items():
+    for key, val in hdulist[0].header.items():
         print(f"{key},{val}")
+
+    data_points = hdulist[0].data
+
+plt.hist(data_points)
+plt.show()
