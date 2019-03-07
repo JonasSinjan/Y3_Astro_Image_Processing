6e367506f4a8bf3afe07d653b27e7bfd3fb36597
Jonas Sinjan <33846422+JonasSinjan@users.noreply.github.com>
2019-03-07 15:12:59 +0000

    removing the edges, top and bottom

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from astropy.io import fits
 import numpy as np
 import matplotlib.pyplot as plt
+
 data_points = np.array([])
 
 with fits.open("A1_mosaic.fits") as hdulist:
@@ -8,6 +9,10 @@ with fits.open("A1_mosaic.fits") as hdulist:
         print(f"{key},{val}")
 
     data_points = hdulist[0].data
+
+# remove the edges, first and last 150 columns
+
+data_points = data_points[150:-150, 150:-150]
 
 plt.hist(data_points)
 plt.show()
