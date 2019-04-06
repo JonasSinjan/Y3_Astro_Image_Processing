443db7ab1fb74d764298ac27d2846beea6619803
Jonas Sinjan <einsteinisbae@gmail.com>
2019-04-06 13:10:13 +0000

    log10 double object

--- plot.py
+++ plot.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
         img.trim(150)
         img.filter_by_sigma(5)
         fig, ax = plt.subplots(figsize=(6, 11), dpi=800)
-        image_map = ax.imshow((img.data * img.mask), origin="lower", cmap='gray', aspect="equal")
+        image_map = ax.imshow(np.log10(img.data * img.mask), origin="lower", cmap='gray', aspect="equal")
 
         df = pd.read_csv(filename)
         mag = df["Relative Magnitude"]
@@ -21,42 +21,44 @@ if __name__ == '__main__':
         for star in star_points:
             star = np.array(ast.literal_eval(star))
             y_points, x_points = np.transpose(star)
+            #y, x = star[0]
             left = min(x_points) - 1
             bottom = min(y_points) - 1
             right = max(x_points) + 1
             top = max(y_points) + 1
             ax.add_patch(patches.Rectangle((left, bottom), right - left, top - bottom, linewidth=0.8, edgecolor='r',
                                            facecolor="none"))
+            #ax.scatter(np.transpose(x_points) - x + 30, np.transpose(y_points) - y + 30, s=1)
         ax.set_xlabel("X")
         ax.set_ylabel("Y")
         # ax.plot(1450, 3100, color='red', label='Detected Galaxies')
         # ax.legend()
         # fig.colorbar(image_map)
-        #ax.set_ylim(3850, 4040)
-        #ax.set_xlim(300, 500)
+        ax.set_ylim(3920, 3975)
+        ax.set_xlim(380, 440)
         plt.show()
 
-        m = np.arange(10, 25, 0.25)  # need to be wary of this range - could change for different sigma
-        N = [(len(list(filter(lambda x: x < m_i, mag)))) for m_i in m]
-        plt.plot(m, N)
-        plt.xlabel('Magnitude Limit')
-        plt.ylabel('Number of Galaxies')
-        plt.title(f'Number of galaxies against magnitude limit\n{filename}')
-        plt.show()
-
-        m = np.arange(10, 17, 0.5)  # need to be wary of this range - could change for different sigma
-        N = [(len(list(filter(lambda x: x < m_i, mag)))) for m_i in m]
-        plt.figure()
-        slope, intercept, rvalue, pvalue, stderr = linregress(m, np.log10(N))
-        fit_str = f"Linear Regression Fit\nSlope:{round(slope, 3)}±{round(stderr, 3)}\nR^2:{round(rvalue ** 2, 3)}"
-        plt.plot(m, [i * slope + intercept for i in m], 'b-', label=fit_str)
-        plt.plot(m, np.log10(N), 'r.', linestyle='--', label='Raw Data')
-        plt.xlabel('Magnitude Limit')
-        plt.ylabel('Log_10 (Number of Galaxies)')
-
-        plt.title(f'{filename}')
-        plt.legend()
-        plt.show()
+        # m = np.arange(10, 25, 0.25)  # need to be wary of this range - could change for different sigma
+        # N = [(len(list(filter(lambda x: x < m_i, mag)))) for m_i in m]
+        # plt.plot(m, N)
+        # plt.xlabel('Magnitude Limit')
+        # plt.ylabel('Number of Galaxies')
+        # plt.title(f'Number of galaxies against magnitude limit\n{filename}')
+        # plt.show()
+        #
+        # m = np.arange(10, 17, 0.5)  # need to be wary of this range - could change for different sigma
+        # N = [(len(list(filter(lambda x: x < m_i, mag)))) for m_i in m]
+        # plt.figure()
+        # slope, intercept, rvalue, pvalue, stderr = linregress(m, np.log10(N))
+        # fit_str = f"Linear Regression Fit\nSlope:{round(slope, 3)}±{round(stderr, 3)}\nR^2:{round(rvalue ** 2, 3)}"
+        # plt.plot(m, [i * slope + intercept for i in m], 'b-', label=fit_str)
+        # plt.plot(m, np.log10(N), 'r.', linestyle='--', label='Raw Data')
+        # plt.xlabel('Magnitude Limit')
+        # plt.ylabel('Log_10 (Number of Galaxies)')
+        #
+        # plt.title(f'{filename}')
+        # plt.legend()
+        # plt.show()
 
 
     sys.setrecursionlimit(10 ** 5)
